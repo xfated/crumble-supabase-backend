f3a7c184a415dd50064bda29c95330f925c357fc
Kai Yang <taykaiyang556@gmail.com>
2023-05-25 11:47:27 +0800

    add function for autocomplete

---
+++ supabase/functions/autocomplete/index.ts
@@ -0,0 +1,31 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { queryAutocomplete } from '../_places_service/autocomplete_service.ts'
+
+serve(async (req) => {
+  try {
+    const { input } = await req.json()
+    const autocompleteResult = await queryAutocomplete(input)
+
+    return new Response(
+      JSON.stringify(autocompleteResult),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  } catch (error) {
+    console.error(error)
+
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/autocomplete' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"input":"westgate"}'
