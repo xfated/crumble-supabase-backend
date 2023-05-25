3b9e607953fd8c6b51c3d66fda7a461792c490f9
Kai Yang <taykaiyang556@gmail.com>
2023-05-25 11:47:59 +0800

    add function for geocoding get latlong

---
+++ supabase/functions/geocoding-get-latlong/index.ts
@@ -0,0 +1,31 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { queryGeocodingForLatLong } from '../_places_service/geocoding_service.ts'
+
+serve(async (req) => {
+  try {
+    const { address } = await req.json()
+    const geocodingDetails = await queryGeocodingForLatLong(address)
+
+    return new Response(
+      JSON.stringify(geocodingDetails),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  } catch (error) {
+    console.error(error)
+
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/geocoding-get-latlong' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"address":"Gateway Drive, Westgate, Singapore"}'
